58a24f310419b37d8d955d8eaccb337ebfe3646c
devilboyAp <76804851+devilboyAp@users.noreply.github.com>
2025-12-15 16:28:11 +0530

    redo

--- Backend/models/appointment.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose");
-
-module.exports = mongoose.model("Appointment", new mongoose.Schema({
-  patientId: String,
-  date: String,
-  time: String,
-  status: { type: String, default: "Pending" }
-}));
--- Backend/models/user.js
+++
@@ -1,7 +0,0 @@
-const mongoose = require("mongoose");
-
-module.exports = mongoose.model("User", new mongoose.Schema({
-  email: String,
-  otp: String,
-  otpExpiry: Date
-}));
