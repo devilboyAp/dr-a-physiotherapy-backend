e4ecce0cfc3f89aa75b9ebe64b4954cb3ffbbddd
devilboyAp <76804851+devilboyAp@users.noreply.github.com>
2025-12-18 01:30:08 +0530

    Update auth.js

--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
-const User = require("../models/user");
+const User = require("../models/User");
 
 const router = express.Router();
 
@@ -51,3 +51,4 @@ router.post("/login", async (req, res) => {
 });
 
 module.exports = router;
+
