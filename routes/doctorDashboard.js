e66c1363e7139cf354fd774c080982b0fc48c7b9
devilboyAp <76804851+devilboyAp@users.noreply.github.com>
2025-12-18 01:29:19 +0530

    Update doctorDashboard.js

--- routes/doctorDashboard.js
+++ routes/doctorDashboard.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const auth = require("../middleware/auth");
 const Appointment = require("../models/appointment");
-const User = require("../models/user");
+const User = require("../models/User");
 
 // GET Doctor Dashboard
 router.get("/doctor", auth, async (req, res) => {
