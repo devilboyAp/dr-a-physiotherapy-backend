1e5278ae96baa2ac7e5293228486d336e2692b31
devilboyAp <76804851+devilboyAp@users.noreply.github.com>
2025-12-20 02:18:15 +0530

    Update patient.js

--- models/patient.js
+++ models/patient.js
@@ -2,16 +2,23 @@ const mongoose = require("mongoose");
 
 const patientSchema = new mongoose.Schema(
   {
-    name: { type: String, required: true },
-    age: Number,
-    gender: String,
-    phone: String,
-
-    assignedDoctor: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "User"
+    name: {
+      type: String,
+      required: true
     },
-
+    age: {
+      type: Number
+    },
+    gender: {
+      type: String
+    },
+    phone: {
+      type: String,
+      required: true
+    },
+    condition: {
+      type: String
+    },
     createdBy: {
       type: mongoose.Schema.Types.ObjectId,
       ref: "User"
