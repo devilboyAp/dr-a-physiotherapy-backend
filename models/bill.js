6893b9496480cbdb80d29e1f72e3cfecd4d413be
devilboyAp <76804851+devilboyAp@users.noreply.github.com>
2025-12-23 21:47:08 +0530

    Update bill.js

--- models/bill.js
+++ models/bill.js
@@ -1,7 +1,34 @@
 const mongoose = require("mongoose");
 
-module.exports = mongoose.model("Bill", new mongoose.Schema({
-  patientId: String,
-  amount: Number,
-  date: String
-}));
+const billSchema = new mongoose.Schema(
+  {
+    patient: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Patient",
+      required: true
+    },
+    amount: {
+      type: Number,
+      required: true
+    },
+    description: {
+      type: String
+    },
+    paymentStatus: {
+      type: String,
+      enum: ["paid", "unpaid", "partial"],
+      default: "unpaid"
+    },
+    paidAmount: {
+      type: Number,
+      default: 0
+    },
+    createdBy: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User"
+    }
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("Bill", billSchema);
